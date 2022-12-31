02cad67e4820d1df78cc5158deffd9c23f18f589
VikramMor <er.vickymor@gmail.com>
2022-12-31 12:12:59 +0530

    all functions completed and did the test run

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,19 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        SavingsAccount savingsAccount = new SavingsAccount("Vikram", 1000000, 50000, 8);
+        CurrentAccount currentAccount = new CurrentAccount("Vikram Mor", 1000000, "SPRINGBOOT");
+        StudentAccount studentAccount = new StudentAccount("Lakshita", 50000000, "IIT BOMBAY");
+
+        System.out.println(savingsAccount.getSimpleInterest(5));
+        System.out.println(savingsAccount.getMaxWithdrawalLimit());
+
+        System.out.println(currentAccount.generateAccountNumber(12,77));
+        currentAccount.validateLicenseId();
+        System.out.println(currentAccount.getTradeLicenseId());
+
+        System.out.println(studentAccount.getInstitutionName());
     }
 }
